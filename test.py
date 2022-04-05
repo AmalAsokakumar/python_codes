8268212c5acd45deab2eac0eda28d6ebaa8f0ebb
comrider <cena3799@gmail.com>
2022-04-05 17:29:53 +0530

    new test file updated

---
+++ test.py
@@ -0,0 +1,38 @@
+
+def new_game():
+     pass
+def check_answer():
+     pass
+def display_score():
+     pass
+def play_again():
+     pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+questions = {"who created python" : "A", \
+     "what year was python created" : "B", \
+     "python is tributed to which comedy group ?" : "C", \
+     "Is the Earth round ?" : "A" }
+
+# here is a list of list to include all the options 
+options = [["A. Guido van Rossum","B. Elon Musk", "C. Bill Gates","D. Mark Zuckerburg"],
+          ["A. 1989","B. 1991","C. 2000","D. 2016"],
+          ["A. Lonely Island", "B. Smosh", "C. Monty Python","D. SNL"],
+          ["A. True","B. False","C. sometimes","D. what's earth? "]]
